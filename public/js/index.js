626809c62fb3c1fcdc51106e27ee421eb31874e3
davidmorab <davidamorab@gmail.com>
2022-04-04 22:06:47 -0600

    Connected EmailJS to contact.pug
    Added Emailjs functionality in order to receive to an especific admin email the ones sent by customers

--- public/js/index.js
+++ public/js/index.js
@@ -10,3 +10,54 @@ hamburer.addEventListener("click", () => {
 navList.classList.toggle("open");
 });
 }
+
+
+function validate() {
+  let name = document.querySelector(".Contactname");
+  let email = document.querySelector(".Contactemail");
+  let msg = document.querySelector(".Contactmessage");
+  let btn = document.querySelector(".Contactsend");
+
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (name.value == "" || email.value == "" || msg.value == "") {
+      emptyerror();
+    } else {
+      sendmail(name.value, email.value, msg.value);
+      success();
+    }
+  });
+}
+validate();
+
+function sendmail(name, email, msg) {
+  emailjs.send("service_sr9pa0n", "template_2nf7gmu", {
+    to_name: "rentcaproject@gmail.com",
+    from_name: email,
+    message: msg,
+  });
+}
+
+function emptyerror() {
+  Swal.fire({
+    icon: "error",
+    title: "Oops...",
+    text: "Fields cannot be empty!",
+  });
+}
+
+function error() {
+  Swal.fire({
+    icon: "error",
+    title: "Oops...",
+    text: "Something went wrong!",
+  });
+}
+
+function success() {
+   Swal.fire({
+    icon: "success",
+    title: "Success...",
+    text: "Successfully sent message",
+  });
+}
