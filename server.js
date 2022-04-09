780ae520644d27c5704e9b412a79af9fc13a8ad6
davidmorab <davidamorab@gmail.com>
2022-04-08 22:45:35 -0600

    revert commit

--- server.js
+++ server.js
@@ -1,32 +1,10 @@
 const express = require("express");
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const passportLocalMongoose = require("passport-local-mongoose");
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-// const Register = require('./models/registerModel');
-
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'pug');
 
 app.use(express.static('public'));
-
-//Listening to port 3000
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-
-
-// //Connecting Mongodb database
-dotenv.config()
-mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true})
-.then(console.log('Mongodb Database connected'))
-.catch((err) => console.log(err));
-
 
 
 //Rendering localhost:3000/
@@ -61,10 +39,9 @@ app.get("/signup", (req, res) => {
 
 
 
-
-
-
-
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 
 
